79b57c60726eb2a861b459345389e38f4f922a47
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-14 18:18:09 -0500

    add form to show page

--- src/actions.js
+++ src/actions.js
@@ -18,3 +18,4 @@ export const createAction = async ({ request }) => {
     })
     return redirect("/")
 }
+
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom"
+import { Form, useLoaderData } from "react-router-dom"
 
 function Show (props) {
     const cheese = useLoaderData()
@@ -7,7 +7,16 @@ function Show (props) {
             <h1>{cheese.name}</h1>
             <h2>{cheese.countryOfOrigin}</h2>
             <img src={cheese.image} alt={cheese.name}/>
+
+            <h2>Update {cheese.name}</h2>
+            <Form action={`/update/${cheese._id}`} method="post">
+                <input type="input" name="name" defaultValue={cheese.name}/>
+                <input type="input" name="countryOfOrigin" defaultValue={cheese.countryOfOrigin}/>
+                <input type="input" name="image" placeholder="Image of Cheese"/>
+                <input type="submit" value={`Update ${cheese.name}`}/>
+            </Form>
         </div>
+        
     )
 }
 
